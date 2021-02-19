e3c3ed0ba7e27b4930659a244f63b429cdd58534
Justin Payne <justinppayne@gmail.com>
2021-02-18 21:14:16 -0600

    sessionReducer added with set/remove user

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import configureStore from "./store";
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import "./index.css";
 import App from "./App";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
